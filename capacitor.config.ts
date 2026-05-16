b56e462b6e68a0e56283487b5763a7eb41039075
Naufallzz <itsolutionqc@gmail.com>
2026-05-16 10:49:27 +0700

    Fix: Restore branding and workflow configuration

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,11 +2,17 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.rigel.webview',
-  appName: 'Rigel Coins',
+  appName: 'AgencyRigel.com',
   webDir: 'public',
   plugins: {
     PushNotifications: {
       presentationOptions: [],
+    },
+    SplashScreen: {
+      launchShowDuration: 2000,
+      backgroundColor: "#611f95",
+      androidScaleType: "CENTER_INSIDE",
+      showSpinner: false,
     },
   },
   server: {
